e9015c62ce59873e2417537517a14684a33c94eb
jooheekim0713 <jooheekim.dev@gmail.com>
2022-08-23 16:32:16 +0900

    feat : Show video list with title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>app.js</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAgTgdif8AmrvhYsYXZ9_j2BReA_fI44Uk',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
