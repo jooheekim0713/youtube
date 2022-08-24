199461d9b50f8862fcd4beff0117205c06360a68
jooheekim0713 <jooheekim.dev@gmail.com>
2022-08-24 17:42:22 +0900

    refactor : Make youtube class for api control

--- src/App.js
+++ src/App.js
@@ -3,39 +3,17 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
-  const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   const onSearch = (query) => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&maxResults=25&type=video&key=${API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log('error', error));
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAgTgdif8AmrvhYsYXZ9_j2BReA_fI44Uk',
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
