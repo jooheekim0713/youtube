8b92af115a85ad700a6604c643e2301ce84d8de2
jooheekim0713 <jooheekim.dev@gmail.com>
2022-08-25 16:42:09 +0900

    feat : Add responsive mode and clean up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
@@ -11,17 +11,22 @@ function App({ youtube }) {
     setSelectedVideo(video);
   };
 
-  const onSearch = (query) => {
-    setSelectedVideo(null);
-    youtube
-      .search(query) //
-      .then((videos) => setVideos(videos));
-  };
+  const onSearch = useCallback(
+    (query) => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then((videos) => setVideos(videos));
+    },
+    [youtube]
+  );
+
   useEffect(() => {
     youtube
       .mostPopular() //
       .then((videos) => setVideos(videos));
-  }, []);
+  }, [youtube]);
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={onSearch} />
--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,7 +1,7 @@
-import React, { useRef } from 'react';
+import React, { memo, useRef } from 'react';
 import styles from './search_header.module.css';
 
-const SearchHeader = ({ onSearch }) => {
+const SearchHeader = memo(({ onSearch }) => {
   const inputRef = useRef();
   const handleSearch = () => {
     const value = inputRef.current.value;
@@ -41,5 +41,5 @@ const SearchHeader = ({ onSearch }) => {
       </div>
     </header>
   );
-};
+});
 export default SearchHeader;
